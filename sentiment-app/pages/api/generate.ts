a7762534c72b55a29ee471e64026989c182c05b4
Zakwan <hishamzakwan@gmail.com>
2023-01-20 10:23:52 +0800

    Add the sentiment demo

---
+++ sentiment-app/pages/api/generate.ts
@@ -0,0 +1,71 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function(req: any, res: any) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please configure it",
+      }
+    });
+    return;
+  }
+
+  const prompt = req.body.prompt || "";
+  if (prompt.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid prompt",
+      }
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(prompt),
+      temperature: 0,
+      max_tokens: 60,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch (error: any) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: "An error occurred during your request",
+        }
+      });
+    }
+  }
+}
+
+function generatePrompt(prompt: String) {
+  return `Classify the sentiment in these tweets:
+
+1. "I can't stand homework"
+2. "This sucks. I'm bored 😠"
+3. "I can't wait for Halloween!!!"
+4. "My cat is adorable ❤️❤️"
+5. "I hate chocolate"
+6. "${prompt}"
+
+Tweet sentiment ratings:
+1. Negative
+2. Negative
+3. Positive
+4. Positive
+5. Negative
+6.`
+}
---
+++ sentiment-app/pages/index.tsx
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [promptInput, setPromptInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event: any) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt: promptInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setPromptInput("");
+    } catch (error: any) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
+  return (
+    <div>
+      <Head>
+        <title>Sentiment Demo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <img src="twitter-png.gif" className={styles.icon} />
+        <h3>Ask me to rate the sentiment</h3>
+        <h4>This is a sentiment analysis bot, I will rate your tweet </h4>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="prompt"
+            placeholder="Put your tweet here"
+            value={promptInput}
+            onChange={(e) => setPromptInput(e.target.value)}
+          />
+          <input type="submit" value="Rate my tweet" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  )
+}
